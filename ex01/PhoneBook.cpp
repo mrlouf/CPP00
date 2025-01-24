d409f2a3c775b1a31f001d7580a90c6d36a881c5
nponchon <nponchon@student.42.fr>
2025-01-24 17:43:30 +0100

    get index okay

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,8 +6,57 @@
 /*   By: nponchon <nponchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 12:00:06 by nponchon          #+#    #+#             */
-/*   Updated: 2025/01/24 13:50:17 by nponchon         ###   ########.fr       */
+/*   Updated: 2025/01/24 17:42:20 by nponchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
+
+int PhoneBook::getIndex( void ) {
+	
+	std::string input;
+	int index;
+
+    std::cout << "36 15 Minitel> enter an index: ";
+    std::getline(std::cin, input);
+
+	if (input.find_first_not_of("0123456789+-") != std::string::npos) {
+		std::cerr << "Invalid input: only digits and '+' or '-' signs are allowed" << std::endl;
+		return (-1);
+	}
+
+	std::stringstream ss(input);
+
+	ss >> index;
+
+	try	{
+		if ( index < 0 ) {
+      		throw std::invalid_argument( "index is negative" );
+		}
+		else if ( index > 8 ) {
+        	throw std::invalid_argument( "index is out of range" );
+		}
+	}
+	catch(const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return (-1);
+	}
+
+	return index;
+};
+
+void PhoneBook::displayContact(int index) {
+	
+	std::cout << "Displaying contact " << index << std::endl;
+
+};
+
+void PhoneBook::displayAll( void ) {
+	
+	std::cout << "Displaying all contacts \n";
+
+	int index = getIndex();
+	if (index == -1)
+		return;
+	displayContact(index);
+};
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nponchon <nponchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 12:02:39 by nponchon          #+#    #+#             */
-/*   Updated: 2025/01/24 16:02:50 by nponchon         ###   ########.fr       */
+/*   Updated: 2025/01/24 17:40:35 by nponchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,23 @@
 int main( void ) {
 	
     printWelcomeMessage();
-	std::string input = getUserInput();
 
-	//PhoneBook phonebook;
+	PhoneBook book;
 	Contact contact;
 
-	input = getUserInput();
+	std::string input = getUserInput();
 	
 	while (!input.empty()) {
-		if (input.compare("ADD") == 0)
+		if (input.compare("ADD") == 0 || input.compare("add") == 0)
 			contact.addContact();
-		else if (input.compare("SEARCH") == 0)
-			std::cout << "You entered SEARCH" << std::endl;
-		else if (input.compare("EXIT") == 0)
+		else if (input.compare("SEARCH") == 0 || input.compare("search") == 0)
+			book.displayAll();
+		else if (input.compare("EXIT") == 0 || input.compare("exit") == 0)
 			return (0);
-		else
-			std::cout << "Unknown input: \'" << input << "\'" << std::endl;
-		input = getUserInput();
+		else {
+			std::cerr << "36 15 Minitel> Unknown input: \'" << input << "\'" << std::endl;
+			input = getUserInput();
+		};
 	};
 	
 	return (0);
